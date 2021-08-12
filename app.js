786983c21c1e67d82769794ba273d3c63a6f0bc0
DanielRGilles <danryangilles@gmail.com>
2021-08-12 10:55:34 -0700

    refactored my code to not suck so bad

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-import {
-  makeSoundAndChangeH1ForCat,
-  makeSoundAndChangeH1ForDog,
-  makeSoundAndChangeH1ForHorse
-} from './utils.js';
-// grab DOM elements
 
 const horseEl = document.getElementById('horse-image');
 const dogEl = document.getElementById('dog-image');
 const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+const h1El = document.getElementById('animal-name');
 
+function makeSoundAndChangeH1(sound , animalName) {
+    sound.play();
+    h1El.textContent = animalName;
+    setTimeout(() => {
+  h1El.textContent = 'Animal Noises';
+}, 1500);
+}
 
 // set event listeners 
 
 horseEl.addEventListener('click', () => {
-  makeSoundAndChangeH1ForHorse(); // calling my function -- cooking the dish
+  makeSoundAndChangeH1(horseSound, 'horse'); // calling my function 
 });
 
 dogEl.addEventListener('click', () => {
-  makeSoundAndChangeH1ForDog();
+  makeSoundAndChangeH1(dogSound, 'dog');
 });
 
 catEl.addEventListener('click', () => {
-  makeSoundAndChangeH1ForCat();
+  makeSoundAndChangeH1(catSound, 'cat');
 });
 document.addEventListener('keydown', (event) => {
   if (event.key === 'c') {
-    makeSoundAndChangeH1ForCat();
+    makeSoundAndChangeH1();
   }
 
   if (event.key === 'h') {
-    makeSoundAndChangeH1ForHorse();
+    makeSoundAndChangeH1();
   }
 
   if (event.key === 'd') {
-    makeSoundAndChangeH1ForDog();
+    makeSoundAndChangeH1();
   }
 });
